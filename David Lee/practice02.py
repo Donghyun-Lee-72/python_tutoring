cf91f8a30442a1d93762a8c154bd44769b2746fe
david8255 <davidminsup@gmail.com>
2020-12-22 23:43:58 -0600

    David Lee practice 2장 #1~6

--- David Lee/practice02.py
+++ David Lee/practice02.py
@@ -1,36 +1,65 @@
 # Question 1
 if __name__ == "__main__":
     # 코드 작성
+    Korean = 80
+    English = 75
+    Math = 55
+    Average = (Korean + English + Math)/3
+    print(Average)
     pass
 
 # Question 2
 if __name__ == "__main__":
     # 코드 작성
+    num = 13
+    if 13 % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
     pass
 
 # Question 3
 if __name__ == "__main__":
     # 코드 작성
+    a = "8811201068234"
+    YY = "19" + a[0:2]
+    MM = a[2:4]
+    DD = a[4:6]
+    ssn_1 = YY + MM + DD
+    ssn_2 = a[6:]
+    print(ssn_1)
+    print(ssn_2)
     pass
 
 # Question 4
 if __name__ == "__main__":
     # 코드 작성
+    pin = "881120-1068234"
+    print(pin[-1])
     pass
 
 # Question 5
 if __name__ == "__main__":
     # 코드 작성
+    a = "a:b:c:d"
+    ans = a.replace(":", "#")
+    print(ans)
     pass
 
 # Question 6
 if __name__ == "__main__":
     # 코드 작성
+    a = [1,3,5,4,2]
+    a.sort()
+    a.reverse()
+    print(a)
     pass
 
 # Question 7
 if __name__ == "__main__":
     # 코드 작성
+
     pass
 
 # Question 8
