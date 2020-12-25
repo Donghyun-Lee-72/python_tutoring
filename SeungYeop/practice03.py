1e2cbed800c9228c7e1ac1f091b1ee553945f5e6
yobayaga <ksy5318@gmail.com>
2020-12-25 12:59:06 +0900

    practice commit

--- SeungYeop/practice03.py
+++ SeungYeop/practice03.py
@@ -1,7 +1,15 @@
 if __name__ == "__main__":
-    # Q1
+    # Q1 answer = shirt
 
     # Q2
+    num = 0
+    answer = 0
+    while num <=1000:
+        if num % 3 == 0:
+            answer = answer + num
+        num += 1
+
+    print(answer)
 
     # Q3
 
