0079c8f978ee7f1d95b7f672350452145febad14
yobayaga <ksy5318@gmail.com>
2020-12-25 14:37:26 +0900

    practice commit

--- SeungYeop/practice03.py
+++ SeungYeop/practice03.py
@@ -12,11 +12,23 @@ if __name__ == "__main__":
     print(answer)
 
     # Q3
+    a = 1
+    star = ""
+    while a <= 5:
+        star += "*"
+        print (star)
+        a += 1
 
     # Q4
+    for i in range(101): print (i)
 
     # Q5
-
+    list = [70,60,55,75,95,90,80,80,85,100]
+    total = 0
+    for i in list:
+        total = total + i
+print (total / len(list))
     # Q6
+for n in [1,2,3,4,5]: if n% 2 ==1: print (n*2)
 
-    pass
+pass
